7cb0dec36cc9ee94dea77b51ed2847a04f08b133
ShonRepo <ivan_n6_20@icloud.com>
2020-08-11 22:32:50 +0300

    update

--- server.js
+++ server.js
@@ -1,19 +1,11 @@
-//Install express server
 const express = require('express');
-const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static('./dist/angular-heroku'));
+
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/angular-heroku/'}),
+);
 
 app.listen(process.env.PORT || 8080);
-
-app.get('/*', function(req,res) {
-
-  res.sendFile(path.join(__dirname+'/dist/index.html'));
-});
-
-
-console.log("Its Work!")
-// Start the app by listening on the default Heroku port
